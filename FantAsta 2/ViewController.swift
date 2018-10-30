82418221600f9ffdf17ba801c7cc91ba6b576296
Patrizio Pezzilli <patriziopezzilli@gmail.com>
2018-10-30 21:19:45 +0100

    Initial Commit

---
+++ FantAsta 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FantAsta 2
+//
+//  Created by Patrizio Pezzilli on 30/10/2018.
+//  Copyright © 2018 Patrizio Pezzilli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
